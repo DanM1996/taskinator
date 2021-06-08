0613fd38fab9f1f228955532450b4b3e7efd7b32
Dan <bmdmartinez@gmail.com>
2021-06-08 11:37:28 -0400

    submission form added and stops browswer default action to refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do"); // defines the ul id as a variable
 
 var createTaskHandler = function() { // place before listener so function can be defined
+    event.preventDefault();
+
     var listItemEl = document.createElement("li"); //creates a new list item element
     listItemEl.className = "task-item"; //takes the new list item and assigns it the css class
     listItemEl.textContent = "This is a new task."; // adds text to the new list item
     tasksToDoEl.appendChild(listItemEl); // attaches the list item to the bottom of the unordered list
 };
 
-buttonEl.addEventListener("click", createTaskHandler) 
+formEl.addEventListener("submit", createTaskHandler); 
