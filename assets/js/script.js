59a06b18b01a636dc9472438624a0f92c70c06ab
Dan <bmdmartinez@gmail.com>
2021-06-08 12:34:21 -0400

    Add ability to submit custom task Form refreshes after ever submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,12 @@ var taskFormHandler = function(event) { // place before listener so function can
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    // check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput){
+        alert("You need to fill out the task form!");
+        return false;
+    }
+    formEl.reset();
     // package up data as an object
     var taskDataObj = {
         name: taskNameInput,
