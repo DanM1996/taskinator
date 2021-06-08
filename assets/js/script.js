500108bd4e1b79c9bf4af1104ce7232a775755dd
Dan <bmdmartinez@gmail.com>
2021-06-08 11:15:33 -0400

    added form submit and dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do"); // defines the ul id as a variable
 
-var createTaskHandler = function() {
+var createTaskHandler = function() { // place before listener so function can be defined
     var listItemEl = document.createElement("li"); //creates a new list item element
     listItemEl.className = "task-item"; //takes the new list item and assigns it the css class
     listItemEl.textContent = "This is a new task."; // adds text to the new list item
